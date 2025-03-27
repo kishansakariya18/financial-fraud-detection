82a387322df5c4a37cb1a549ad19803f6b46672f
Sujay Kesariya <sujay@sourcecodelab.co>
2025-03-27 16:27:46 +0530

    double api call on first time rendering solved

--- src/components/ui/useTable.jsx
+++ src/components/ui/useTable.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import useDeepCompareEffect from 'use-deep-compare-effect'
+
 const useTable = ({
   columns,
   fetchData,
@@ -54,7 +56,7 @@ const useTable = ({
   };
 
   // Re-fetch data when pagination or queryParams change
-  useEffect(() => {
+  useDeepCompareEffect(() => {
     fetchTableData();
     const pageIndex = isNaN(queryParams.pageIndex) ? 0 : +queryParams.pageIndex;
     const pageSize = isNaN(queryParams.pageSize) ? 10 : +queryParams.pageSize;
@@ -64,7 +66,6 @@ const useTable = ({
       pageIndex,
       pageSize,
     });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [queryParams]);
 
   //   Sync pagination with URL
@@ -73,8 +74,7 @@ const useTable = ({
       ...Object.fromEntries(prevParams), // Preserve existing query params
       pageIndex: pagination.pageIndex,
       pageSize: pagination.pageSize,
-    }));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    }), { replace: true });
   }, [pagination.pageIndex, pagination.pageSize]);
 
   const table = useReactTable({
--- src/utils/custom.utilities.js
+++ src/utils/custom.utilities.js
@@ -1,32 +1,34 @@
-import moment from "moment-timezone";
+import moment from 'moment-timezone';
 
 export function getQueryParams(searchParams) {
-    const result = Object.fromEntries([...searchParams]);
-    return result;
+  const result = Object.fromEntries([...searchParams]);
+  if (!result.pageIndex) {
+    result.pageIndex = '0';
   }
 
-
-  export function isEmptyObject(obj) {
-    return !(Object.keys(obj).length > 0);
-  }
-  
-
-
-
-  export function replaceText(text, replaceTo, replaceWith) {
-    var regex = new RegExp(replaceTo, 'g');
-    return text.replace(regex, replaceWith);
+  if (!result.pageSize) {
+    result.pageSize = '10';
   }
 
+  return result;
+}
 
-  export const ConvertDateIntoUTC = (date) => {
-    console.log('typeoftypeoftypeof<<>>', typeof date, '   ', date);
-    if (date) {
-      if (typeof date == 'string') {
-        date = moment(date);
-      }
-      return moment.utc(date).format('YYYY-MM-DD HH:mm:ss');
+export function isEmptyObject(obj) {
+  return !(Object.keys(obj).length > 0);
+}
+
+export function replaceText(text, replaceTo, replaceWith) {
+  var regex = new RegExp(replaceTo, 'g');
+  return text.replace(regex, replaceWith);
+}
+
+export const ConvertDateIntoUTC = (date) => {
+  console.log('typeoftypeoftypeof<<>>', typeof date, '   ', date);
+  if (date) {
+    if (typeof date == 'string') {
+      date = moment(date);
     }
-    return '';
-  };
-  
+    return moment.utc(date).format('YYYY-MM-DD HH:mm:ss');
+  }
+  return '';
+};
