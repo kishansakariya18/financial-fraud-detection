64ebeec539e16f89cfa39e00dcda35c73774145b
Aadi Shukla <aadi@sourcecodelab.co>
2025-07-28 15:32:49 +0530

    removed login validation

--- src/app/pages/Auth/schema.js
+++ src/app/pages/Auth/schema.js
@@ -6,13 +6,14 @@ export const loginSchema = Yup.object().shape({
     .required('Mobile Is Required')
     .matches(/^(\+\d{1,3}[- ]?)?\d{10}$/, 'Invalid Mobile Number')
     .length(10, 'Mobile Length Must Be 10'),
-  password: Yup.string()
-    .required('Password is required')
-    .min(8, 'Password must be at least 8 characters')
-    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .matches(/[0-9]/, 'Password must contain at least one number')
-    .matches(/[^A-Za-z0-9]/, 'Password must contain at least one special character')
+  password: Yup.string().trim().required('Password Is Required')
+  // password: Yup.string()
+  //   .required('Password is required')
+  //   .min(8, 'Password must be at least 8 characters')
+  //   .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  //   .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+  //   .matches(/[0-9]/, 'Password must contain at least one number')
+  //   .matches(/[^A-Za-z0-9]/, 'Password must contain at least one special character')
 });
 export const otpVerificationSchema = Yup.object().shape({
   otp: Yup.string().trim().required('OTP Is Required').length(6, 'OTP Length Must be 6')
