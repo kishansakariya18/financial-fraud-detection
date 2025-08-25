85a0de46839305e45f2b9e079cb390a037b450ee
Aadi Shukla <aadi@sourcecodelab.co>
2025-08-25 19:22:21 +0530

    category name validation added

--- src/app/pages/casino-management/category/validate.js
+++ src/app/pages/casino-management/category/validate.js
@@ -2,5 +2,13 @@ import * as Yup from 'yup';
 
 export const validateSchema = Yup.object().shape({
   // Personal Information
-  name: Yup.string().trim().required('Category Name Required')
+  name: Yup.string()
+    .trim()
+    .required('Category name is required')
+    .min(3, 'Category name must be at least 3 characters')
+    .max(20, 'Category name must be at most 20 characters')
+    .matches(
+      /^[A-Za-z0-9 _.-]+$/,
+      'Only letters, numbers, spaces, dot, dash and underscore are allowed'
+    )
 });
