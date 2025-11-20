90cbcac16b4b68067f845920f97009d2a7c1991c
kishan <kishan@sourcecodelab.co>
2025-11-20 17:54:12 +0530

    timezone code added

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -9,8 +9,8 @@ axios.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
     config.headers.Token = `${token}`;
-    config.headers['time-zone'] = Intl.DateTimeFormat().resolvedOptions().timeZone;
   }
+  config.headers['time-zone'] = Intl.DateTimeFormat().resolvedOptions().timeZone;
   return config;
 });
 
