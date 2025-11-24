cceeb6d25bd0df4816b9bc29cddbbf6fb3498e0e
kishan <kishan@sourcecodelab.co>
2025-11-24 13:01:33 +0530

    ui bugs resolved

--- src/components/sections/player-management/helper.js
+++ src/components/sections/player-management/helper.js
@@ -87,12 +87,12 @@ export const playerStatusToAPI = (status) => {
   switch (status) {
     case 'active':
       return 1;
-    case 'inactive':
-      return 0;
+    // case 'inactive':
+    //   return 0;
     case 'blocked':
       return 2;
     default:
-      return null;
+      return 2;
   }
 };
 export const playerStatusOptions = [
@@ -102,12 +102,12 @@ export const playerStatusOptions = [
     color: 'success',
     icon: CheckBadgeIcon
   },
-  {
-    value: 'inactive',
-    label: 'Inactive',
-    color: 'error',
-    icon: XCircleIcon
-  },
+  // {
+  //   value: 'inactive',
+  //   label: 'Inactive',
+  //   color: 'error',
+  //   icon: XCircleIcon
+  // },
   {
     value: 'blocked',
     label: 'Blocked',
