3c0127f760f15a733531cf8e4d63f746800ca77c
Aadi Shukla <aadi@sourcecodelab.co>
2025-12-04 12:55:57 +0530

    hidden inquiry module

--- src/app/navigation/dashboards.js
+++ src/app/navigation/dashboards.js
@@ -19,7 +19,7 @@ import { siteConfiguration } from './site-configuration';
 import { bonusManagement } from './bonus-management';
 import { userClass } from './user-class';
 import { responsibleGambling } from './responsible-gambling';
-import { enquires } from './enquires';
+// import { enquires } from './enquires';
 import { blacklist } from './blacklist';
 import { bank } from './bank';
 import { userManualWithdrawTransaction } from './user-manual-withdraw-transaction';
@@ -75,7 +75,7 @@ export const dashboards = () => ({
     crm,
     userClass,
     responsibleGambling,
-    enquires,
+    // enquires,
     blacklist,
     bank,
     userManualWithdrawTransaction,
